d22880f78bcd64c4cf09dbd406ed5ebb9285cac6
Abdxrrahman <71223870+Abdxrrahman@users.noreply.github.com>
2021-12-31 15:42:47 +0100

    Animation added and UI modified [31/12/2021]

---
+++ static/js/animation.js
@@ -0,0 +1,4 @@
+
+setTimeout(function(){
+    document.getElementById("animationContainer").style.display = "none";
+},10000)
--- test.py
+++ test.py
@@ -1,8 +1,1 @@
-import requests
-from bs4 import BeautifulSoup
 
-r = requests.get('https://s2dfree.de/MczozMjoiNjIxOXx8ODUuMjI2LjE4MS4yMDZ8fDE2NDA4NzM5NzAiOw.html')
-
-soup = BeautifulSoup(r.text, 'html.parser')
-
-print(soap.prettify())
